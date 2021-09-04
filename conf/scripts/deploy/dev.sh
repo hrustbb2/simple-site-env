9b18788ebf9bef3c9596fcc7d189b6ef3b1619e8
Rustam Batalov <brn@itigro.ru>
2021-09-04 14:00:51 +0500

    Окружение для работы над версткой макетов

---
+++ conf/scripts/deploy/dev.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/hrustbb2/simple-site.git ./app
---
+++ conf/scripts/nodejs/deploy_dev.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd /home/app/resources/assets
+npm install
+npm run buildCSS
